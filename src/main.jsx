dc8a70f0f7d6561cf97ef82b656c01967af96312
italocarlosbezerra <contato.italocarlosbezerra@gmail.com>
2023-04-03 22:08:53 -0300

    estruturando o projeto

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Portfolio from "./pages/Portfolio";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+   <React.StrictMode>
+      <Portfolio />
+   </React.StrictMode>
+);
---
+++ src/pages/Portfolio/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function Portfolio() {
+   return <div className="App">Teste</div>;
+}
+
+export default Portfolio;
